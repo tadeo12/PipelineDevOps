c3a884c5806efef0407a0d9c1e406c1ea02edd5d
unknown <tadeoova@gmail.com>
2023-03-03 15:14:26 -0300

    correcion funcion main para test unitario

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ if not os.path.exists('build'):
     os.makedirs('build')
 
 # Define the content of the HTML file
-HTML_CONTENT = '''
-<!DOCTYPE html>
-<html>
-<head>
-    <title>My Website</title>
-</head>
-<body>
-    <h1>Welcome to my website!</h1>
-    <p>Thanks for visiting.</p>
-</body>
-</html>
-'''
+HTML_CONTENT = (
+    '<!DOCTYPE html>\n'
+    '<html>\n'
+    '<head>\n'
+    '    <title>My Website</title>\n'
+    '</head>\n'
+    '<body>\n'
+    '    <h1>Welcome to my website!</h1>\n'
+    '    <p>Thanks for visiting.</p>\n'
+    '</body>\n'
+    '</html>\n'
+)
 
 # Write the content to the index.html file in the 'build' directory
 with open('build/index.html', 'w', encoding='utf-8') as f:
