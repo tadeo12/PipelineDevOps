7c156a24b615217146793aba356d97893c35434a
unknown <tadeoova@gmail.com>
2023-03-03 15:17:42 -0300

    lint espacio al final del archivo

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
-"""
-This module creates a 'build' directory if it doesn't exist
-and writes a simple "index.html" file in it.
-"""
-
 import os
 
-# Create a 'build' directory if it doesn't exist
-if not os.path.exists('build'):
-    os.makedirs('build')
 
-# Define the content of the HTML file
-HTML_CONTENT = (
-    '<!DOCTYPE html>\n'
-    '<html>\n'
-    '<head>\n'
-    '    <title>My Website</title>\n'
-    '</head>\n'
-    '<body>\n'
-    '    <h1>Welcome to my website!</h1>\n'
-    '    <p>Thanks for visiting.</p>\n'
-    '</body>\n'
-    '</html>\n'
-)
+def create_index_html():
+    # Create a 'build' directory if it doesn't exist
+    if not os.path.exists('build'):
+        os.makedirs('build')
 
-# Write the content to the index.html file in the 'build' directory
-with open('build/index.html', 'w', encoding='utf-8') as f:
-    f.write(HTML_CONTENT)
+    # Define the content of the HTML file
+    HTML_CONTENT = '''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>My Website</title>
+    </head>
+    <body>
+        <h1>Welcome to my website!</h1>
+        <p>Thanks for visiting.</p>
+    </body>
+    </html>
+    '''
 
-print('index.html file created in build directory.')
+    # Write the content to the index.html file in the 'build' directory
+    with open('build/index.html', 'w', encoding='utf-8') as f:
+        f.write(HTML_CONTENT)
+
+    print('index.html file created in build directory.')
+
+
+def main():
+    create_index_html()
+
+
+if __name__ == '__main__':
+    main()
